e357c076c0f5e09b4a492730d8721e35fef4486f
rnrn4728@gmail.com <rnrn4728@gmail.com>
2024-02-08 12:58:02 +0900

    [Repository 생성] 1. Book Repository 생성

---
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository { // JpaRepository --> CRUD Method 제공
+
+}
