eab1cb13518e3939b81dfab5bd94ada9a475456d
rnrn4728@gmail.com <rnrn4728@gmail.com>
2024-02-08 12:47:54 +0900

    [Entity 생성] 1. Book Entity 생성 2. DB Table 생성 연동

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book { // Book --> Table
+
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AutoIncrement(AI)
+    private int id; // 일련번호 : 1 2 3 4
+    @Column(length = 50, nullable = false) // 길이 지정, null 허용 x
+    private String title; // 제목
+    private int price; // 가격
+    private String name; // 저자명
+    private int page; // 페이지수
+
+}
