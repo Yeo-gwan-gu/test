7c429a219c9377a427d17b04cba7fc77c77f0378
ygg <rnrn4728@gmail.com>
2024-02-13 11:44:05 +0900

    [Controller] - 특정 레코드 가져오기

--- src/main/java/com/example/demo/controller/BookRestController.java
+++ src/main/java/com/example/demo/controller/BookRestController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.Book;
 import com.example.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,17 @@ public class BookRestController {
     @GetMapping("/books")
     public List<Book> getList() {
         return service.getList();
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<?> getById(
+            @PathVariable Long id
+    ) {
+        try {
+            return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>("Book not found with id" + id, HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/books")
