c55f6eb9f377c397cec59e9301dfdd5dc90a895b
ygg <rnrn4728@gmail.com>
2024-02-13 11:34:37 +0900

    [Repository] - 쿼리메서드 설명 주석 추가

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRepository --> CRUD Method 제공
 public interface BookRepository extends JpaRepository<Book, Long> { // Table 정보, PK 타입 지정
     // R(Read) : 전체 리스트 조회 -> findALl()
-    // R(Read) : 특정 레코드 가져오기 -> findById( PK값 )
+    // R(Read) : 특정 레코드 가져오기
+    // 1. Id(PK) 값으로 레코드 가져오기 -> findById( PK값 )
+    // 2. Id가 아닌 다른 레코드 가져올 경우 : 쿼리메서드(규칙) 사용 -> findBy+속성명
+    // -> ex) findByTitle(String title), findByTitleANDPrice(String title, int price)
+    // 3. 사용자 정의 쿼리문 메서드 생성
+    // @Query("select * from book")
     // C(Create) : 데이터 입력하기 -> save()
 }
