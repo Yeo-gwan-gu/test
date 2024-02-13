594513b3142b71c8e0fe552fc52819847877d53e
ygg <rnrn4728@gmail.com>
2024-02-13 11:25:36 +0900

    [Repository] - 특정 데이터 가져오기

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRepository --> CRUD Method 제공
 public interface BookRepository extends JpaRepository<Book, Long> { // Table 정보, PK 타입 지정
     // R(Read) : 전체 리스트 조회 -> findALl()
+    // R(Read) : 특정 레코드 가져오기 -> findById( PK값 )
     // C(Create) : 데이터 입력하기 -> save()
 }
