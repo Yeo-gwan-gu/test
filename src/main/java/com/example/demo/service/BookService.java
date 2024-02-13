f2661e037fc7741119ccef6851651fc3795dea8e
ygg <rnrn4728@gmail.com>
2024-02-13 11:39:41 +0900

    [Service] - 특정 레코드 가져오기

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service // 트랜잭션 처리
 public class BookService {
@@ -16,6 +17,17 @@ public class BookService {
     // 전체 리스트 조회
     public List<Book> getList() {
         return repository.findAll();
+    }
+
+    // 특정 레코드 가져오기
+    public Book getById(Long id) {
+        Optional<Book> optional = repository.findById(id);
+        // id 값 존재 여부 검증
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            throw new RuntimeException("Book not found with id: " + id);
+        }
     }
 
     // 데이터 입력하기
