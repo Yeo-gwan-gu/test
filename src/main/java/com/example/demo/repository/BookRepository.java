66b9b029e367cbf0d09e085bc15c9b2a8d8c4c26
ygg <rnrn4728@gmail.com>
2024-02-13 10:32:47 +0900

    [Repository 생성] 2. Table 정보, PK 타입 지정

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository { // JpaRepository --> CRUD Method 제공
-    
+// JPA
+// JpaRepository --> CRUD Method 제공
+public interface BookRepository extends JpaRepository<Book, Long> { // Table 정보, PK 타입 지정
+
 }
