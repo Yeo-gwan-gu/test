680c58ea096bf1d315df505017d43fe06b4ec03e
ygg <rnrn4728@gmail.com>
2024-02-13 11:11:00 +0900

    [Repository] 1. 데이터 입력하기

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JPA
 // JpaRepository --> CRUD Method 제공
 public interface BookRepository extends JpaRepository<Book, Long> { // Table 정보, PK 타입 지정
-    // 전체 리스트 조회 -> findALl()
-
+    // R(Read) : 전체 리스트 조회 -> findALl()
+    // C(Create) : 데이터 입력하기 -> save()
 }
