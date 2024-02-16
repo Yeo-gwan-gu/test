2becd1c9e6e9bc4c49f1809e53f0142aa45f7d03
ygg <rnrn4728@gmail.com>
2024-02-16 15:46:51 +0900

    [Service & interface] - 회원가입 서비스 생성 - 회원가입 인터페이스 생성

---
+++ src/main/java/com/example/demo/service/RoleService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Role;
+import com.example.demo.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+    public Role findByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
